dc6a9b3b5f40092321ceb2ee173b8274cc222a7b
Zachary Drummond <drummond.zachary@pm.me>
2023-06-20 23:19:40 -0400

    WIP: add main.cpp file for benchmarking C++ interface.

---
+++ src/cpp_abstract_class/bin/main.cpp
@@ -0,0 +1,11 @@
+// Project includes.
+#include <rand_interface.hpp>
+#include <xoshiro256p_rand.hpp>
+#include <xoshiro256ss_rand.hpp>
+// Library includes.
+#include <iostream>
+
+int main(void)
+{
+	return EXIT_SUCCESS;
+}
